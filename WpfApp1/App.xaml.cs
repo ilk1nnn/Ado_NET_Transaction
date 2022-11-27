079fd94bc1b31b056983cd6f04ca9b10ac04bf01
Sule_jr92 <Sule_jr92@STHQ0129-04>
2022-11-27 15:58:03 +0400

    Add project files.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string ConnectionStringSql = "Data Source=STHQ0129-04;Initial Catalog=Library;User ID=admin;Password=admin;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //using (var connection = new SqlConnection())
+            //{
+
+            //    connection.ConnectionString = App.ConnectionStringSql;
+            //    connection.Open();
+            //    DataSet set = new DataSet();
+            //    var da = new SqlDataAdapter("SELECT Id,Name,Pages,YearPress FROM Books",connection);
+
+            //    da.Fill(set,"Books");
+
+            //    DataViewManager dvm = new DataViewManager(set);
+            //    dvm.DataViewSettings["Books"].RowFilter = "Pages>500";
+            //    //dvm.DataViewSettings["Books"].RowFilter = "YearPress>2000";
+            //    //dvm.DataViewSettings["Books"].Sort = "YearPress DESC";
+
+            //    DataView dv = dvm.CreateDataView(set.Tables["Books"]);
+
+
+            //    data_grid.ItemsSource = dv;
+
+            //    //data_grid.ItemsSource = set.Tables[0].DefaultView;
+
+            //}
+
+
+
+            // ------------------------------------------------------------------------------------------
+
+
+            #region Transaction
+
+            SqlTransaction sqlTransaction = null;
+            using (var connection = new SqlConnection())
+            {
+
+                connection.ConnectionString = App.ConnectionStringSql;
+                connection.Open();
+
+
+                sqlTransaction = connection.BeginTransaction();
+
+                SqlCommand comm1 = new SqlCommand("INSERT INTO Press(Id,Name) VALUES(@id,@name)",connection);
+
+                comm1.Transaction = sqlTransaction;
+
+                comm1.Parameters.Add(new SqlParameter()
+                {
+                    SqlDbType = SqlDbType.Int,
+                    Value = 5555,
+                    ParameterName = "@id"
+                });
+
+                comm1.Parameters.Add(new SqlParameter()
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    Value = "John",
+                    ParameterName = "@name"
+                });
+
+
+                SqlCommand comm2 = new SqlCommand("sp_UpdateBook",connection);
+                comm2.Transaction = sqlTransaction;
+                comm2.CommandType = CommandType.StoredProcedure;
+
+                var p1 = new SqlParameter();
+                p1.Value = 1;
+                p1.ParameterName = "@My_Id";
+                p1.SqlDbType = SqlDbType.Int;
+
+                var p2 = new SqlParameter();
+                p2.Value = 2345;
+                p2.ParameterName = "@Page";
+                p2.SqlDbType = SqlDbType.Int;
+
+
+                comm2.Parameters.Add(p1);
+                comm2.Parameters.Add(p2);
+
+
+
+                try
+                {
+                    comm1.ExecuteNonQuery();
+                    comm2.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                    MessageBox.Show("OKAY");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    sqlTransaction.Rollback();
+                }
+
+
+
+            }
+
+
+                #endregion
+
+            }
+    }
+}
